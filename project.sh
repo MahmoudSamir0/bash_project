92d210487be9a0c1e57c271f11ba66338aa9df42
MahmoudSamir0 <samermahmoud142@gmail.com>
2022-12-12 19:24:07 +0200

    try number 2

--- project.sh
+++ project.sh
@@ -1,19 +1,48 @@
-#!/usr/bin/bash 
-while true
-read -p " enter your database name " $name
-case $name in 
-    [0-9])
-        echo "the Databse name start with number "
-        echo "wrong name"
-    ;;
-    +[.,!#$%^&*_-?/><,+:;]) )
+#!/usr/bin/bash
+export LC_COLLATE=C
+shopt -s extglob
 
-    *)
-        echo "Default" 
-esac 
-     if [ -e $name ];then
-     echo "there a database have Same Name please change the name "
-     else 
-     mkdir $name
-     fi
-done 
+read -p 'enter your database name ' name
+case $name in 
+    +([0-9]) )
+        echo "the Databse name start with number "
+        echo "wrong name" exit
+        
+    ;;
+    +(["."][","]["#"]["%"]["&"]["^"]["*"]["_"]["-"]["="]["+"][";"][":"]['/']['!']) )
+         echo "name of database contains special characters"
+         exit 
+    ;;
+     [" "])
+         echo "name of database spaces" exit
+         
+    ;;
+    +([a-zA-Z]) )
+    if [ -e $name ];then
+         read -p "there a database have Same Name please change the name or do you want to open it" answer
+             case $answer in 
+             "yes" )
+             cd $HOME/$name
+             ;;
+              "no" )
+            exit
+             ;;   
+             *) 
+            esac
+    else
+    mkdir $name
+    echo "your database is here now "
+    read -p "do you want to open your database"  ans
+     case $answer in 
+             "yes" )
+             cd $HOME/$name
+             ;;
+              "no" )
+              exit
+             ;;   
+             *)  
+             esac
+    fi
+     ;;
+   *)
+    esac 
