d6af5859d1328624603fb938a00ef5ebf10f3dcc
MahmoudSamir0 <samermahmoud142@gmail.com>
2022-12-13 21:23:26 +0200

    day 2

--- mainpart.sh
+++ mainpart.sh
@@ -16,29 +16,160 @@ case $name in
     ;;
     +([a-zA-Z]) )
     if [ -e $name ];then
-         read -p "there a database have Same Name please change the name or do you want to open it press yes to open it or no to exit "  answer
-             case $answer in 
-             "Y" | "y" | "YES" | "Yes" | "yes")
-               source part1.sh $name
-             ;;
-            "N" | "n" | "No" | "NO" | "no" | "nO")
-            exit
-             ;;   
-             *) 
-            esac
+    read -p "there a database have Same Name please change the name or do you want to open it press yes to open it or no to exit "  answer
+    case $answer in 
+    "Y" | "y" | "YES" | "Yes" | "yes")
+    source cd $HOME/$name
+    select choice in "creat database" "list database" "drop database" "connect database"
+    do
+    case $choice  in
+    "creat database")
+read -p 'enter your database name ' nam
+case $nam in 
+    +([0-9]) )
+         exit 
+    ;;
+    +(["."][","]["#"]["%"]["&"]["^"]["*"]["_"]["-"]["="]["+"][";"][":"]['/']['!']) )
+         exit 
+    ;;
+     [" "])
+          exit
+    ;;
+    +([a-zA-Z]) )
+    if [ -e $nam ];then
+    read -p "there a database have Same Name please change the name or do you want to open it press yes to open it or no to exit "  answe
+    case $answe in 
+    "Y" | "y" | "YES" | "Yes" | "yes")
+    source cd $HOME/$nam
+      ;;
+     "N" | "n" | "No" | "NO" | "no" | "nO")
+      exit
+      ;;   
+      *) 
+    esac
+    else
+    mkdir $nam
+    echo "your database is here now "
+    read -p "do you want to open your new database press y or n"  ans
+     case $ans in 
+      "Y" | "y" | "YES" | "Yes" | "yes" )
+    source cd $HOME/$nam
+    ;;
+   "N" | "n" | "No" | "NO" | "no" | "nO" )
+   exit
+   ;;   
+   *)  
+    esac
+    fi
+     ;;
+   *)
+   echo " error your name contain  special characters or number or spaces "
+    esac 
+     ;;
+     "list database")
+     ls -F|grep /
+    ;;
+    "drop database")
+    read -p" enter name of database " data
+    if [ -d $data ] ; then
+    rm -r $data
+    echo "done"
+    else 
+    echo "This database is not here. Check the database name again"
+    fi
+      ;;
+    "connect database")
+      exit
+      ;;
+      *)
+      echo "Nothing"
+      esac
+      done
+
+      ;;
+     "N" | "n" | "No" | "NO" | "no" | "nO")
+      exit
+      ;;   
+      *) 
+    esac
     else
     mkdir $name
     echo "your database is here now "
-    read -p "do you want to open your database press y or n"  ans
-     case $answer in 
-             "Y" | "y" | "YES" | "Yes" | "yes" )
-               source part1.sh $name
-             ;;
-            "N" | "n" | "No" | "NO" | "no" | "nO" )
-              exit
-             ;;   
-             *)  
-             esac
+    read -p "do you want to open your new database press y or n"  ans
+     case $ans in 
+      "Y" | "y" | "YES" | "Yes" | "yes" )
+    source cd $HOME/$name
+    select choice in "creat database" "list database" "drop database" "connect database"
+    do
+    case $choice  in
+    "creat database")
+    read -p 'enter your database name ' nam
+case $nam in 
+    +([0-9]) )
+         exit 
+    ;;
+    +(["."][","]["#"]["%"]["&"]["^"]["*"]["_"]["-"]["="]["+"][";"][":"]['/']['!']) )
+         exit 
+    ;;
+     [" "])
+          exit
+    ;;
+    +([a-zA-Z]) )
+    if [ -e $nam ];then
+    read -p "there a database have Same Name please change the name or do you want to open it press yes to open it or no to exit "  answe
+    case $answe in 
+    "Y" | "y" | "YES" | "Yes" | "yes")
+    source cd $HOME/$nam
+      ;;
+     "N" | "n" | "No" | "NO" | "no" | "nO")
+      exit
+      ;;   
+      *) 
+    esac
+    else
+    mkdir $nam
+    echo "your database is here now "
+    read -p "do you want to open your new database press y or n"  ans
+     case $ans in 
+      "Y" | "y" | "YES" | "Yes" | "yes" )
+    source cd $HOME/$nam
+    ;;
+   "N" | "n" | "No" | "NO" | "no" | "nO" )
+   exit
+   ;;   
+   *)  
+    esac
+    fi
+     ;;
+   *)
+   echo " error your name contain  special characters or number or spaces "
+    esac 
+     ;;
+    "list database")
+     ls -F|grep /
+    ;;
+    "drop database")
+    read -p" enter name of database " data
+    if [ -d $data ] ; then
+    rm -r $data
+    echo "done"
+    else 
+    echo "This database is not here. Check the database name again"
+    fi
+      ;;
+    "connect database")
+     exit
+      ;;
+      *)
+    echo "Nothing"
+    esac
+    done
+    ;;
+   "N" | "n" | "No" | "NO" | "no" | "nO" )
+   exit
+   ;;   
+   *)  
+    esac
     fi
      ;;
    *)
